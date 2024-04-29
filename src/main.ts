63608a60c2ba2f58c183aebaa22b1afe60752883
NasmaAlabed2002 <nasmaalabednice99@gmail.com>
2024-04-30 00:39:44 +0300

    deploy1

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const port = process.env.PORT || 3200;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
@@ -16,6 +18,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3200);
+  await app.listen(port);
 }
 bootstrap();
