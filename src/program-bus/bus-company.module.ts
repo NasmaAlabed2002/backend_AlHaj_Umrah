8de79d6a9979bce4b03d4814bdfc2ac278abb363
NasmaAlabed2002 <nasmaalabednice99@gmail.com>
2024-05-07 23:56:11 +0300

    programbus31test6

--- src/program-bus/bus-company.module.ts
+++ src/program-bus/bus-company.module.ts
@@ -1,17 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProgramBusService } from './program-bus.service';
-import { ProgramBusController } from './program-bus.controller';
-import { ProgramBus , ProgramBuschema } from './entities/program-bus.entity';
-import { ProgramUmrah, ProgramUmrahchema } from 'src/program_umrah/entities/program_umrah.entity';
 import { busCompany , busCompanySchema } from './entities/bus-company.schema';
 import { Seat , SeatSchema } from './entities/seat.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BusCompanyController } from './bus-company.controller';
 @Module({
   imports: 
   [
     MongooseModule.forFeature([
-      { name:  ProgramBus.name, schema: ProgramBuschema },
-      { name: ProgramUmrah.name, schema: ProgramUmrahchema },
       { name: busCompany.name , schema: busCompanySchema },
       { name: Seat.name , schema: SeatSchema },
     ]),
@@ -25,7 +21,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 //     } 
 //   }  
 // ])],
-  controllers: [ProgramBusController],
+  controllers: [ BusCompanyController],
+
   providers: [ProgramBusService],
 })
 export class ProgramBusModule {}
