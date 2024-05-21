53d43dfab354c27ad0d4a05b8b2567a45ac52239
NasmaAlabed2002 <nasmaalabednice99@gmail.com>
2024-05-22 00:29:40 +0300

    7

--- src/cloudinary/cloudinary.module.ts
+++ src/cloudinary/cloudinary.module.ts
@@ -4,11 +4,7 @@ import { CloudinaryService } from './clodinary.service';
 import { CloudinaryController } from './cloudinary.controller';
 import { MulterModule } from '@nestjs/platform-express';
 @Module({
-  imports: [
-    MulterModule.register({
-      dest: './picture', // المسار الذي سيتم تخزين الملفات المرفوعة إليه
-    }),
-  ],
+
   controllers :[CloudinaryController],
   providers: [CloudinaryProvider, CloudinaryService],
   exports: [CloudinaryProvider, CloudinaryService],
