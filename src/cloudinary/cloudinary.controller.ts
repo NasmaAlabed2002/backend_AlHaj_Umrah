e61c19ce5cec48fe73f24828165e3e6bd2184f5b
NasmaAlabed2002 <nasmaalabednice99@gmail.com>
2024-05-22 23:45:46 +0300

    14

--- src/cloudinary/cloudinary.controller.ts
+++ src/cloudinary/cloudinary.controller.ts
@@ -25,7 +25,9 @@ export class CloudinaryController {
   })
   async uploadImage(@Req() req):Promise<any>
   {
+    console.log("before req");
     const data = await req.file();
+    console.log("after req");
     const url = await this.cloudinaryService.uploadImage(data.file) as any;
     const trimUrl = url.substring(48);
     return('https://res.cloudinary.com/Almutmer/image/upload/q_20/' + trimUrl)
